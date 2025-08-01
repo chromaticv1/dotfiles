4d50fe413e446a3256a588c4fc8532835ee2ad2b
Khalid Muntasir <kmuntasirsawad@gmail.com>
2025-08-02 01:47:45 +0600

    added file directories

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+alias i="sudo pacman -S"
+alias cwp="python .config/myscripts/wallpaper.py"
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
